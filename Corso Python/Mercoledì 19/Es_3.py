4d47f85dfe8f0fe50b9f7f4d0b4d374b6f2a9264
DaniFila <150461747+DaniFila@users.noreply.github.com>
2024-06-19 14:38:45 +0200

    Add files via upload

---
+++ Corso Python/Mercoledì 19/Es_3.py
@@ -0,0 +1,23 @@
+età = input("quanti hanni hai? ")
+if età < "18":
+    print("Mi dispiace, non puoi vedere questo film")
+else:
+    print("puoi vedere questo film")
+
+
+
+a = int(input("indicare il primo numero "))
+b = int(input("indicare il secondo numero "))
+
+print(a+b)
+
+print(a-b)
+
+print(a*b)
+
+if b==0:
+    print("Errore, divisone per zero")
+else:
+    print(a/b)
+
+
