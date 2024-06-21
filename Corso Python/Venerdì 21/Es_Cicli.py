15e33dc093ec17c414c5d13b13443819a9077d0c
DaniFila <150461747+DaniFila@users.noreply.github.com>
2024-06-21 14:25:29 +0200

    Add files via upload

--- Corso Python/Venerdì 21/Es_Cicli.py
+++ Corso Python/Venerdì 21/Es_Cicli.py
@@ -1,34 +1,49 @@
+print("\nMenu'")
+print("Punto 1: ")
+print("Punto 2: ")
+print("Punto 3: ")
+print("Exit: (qualsiasi tasto): ")
+
+
+
 # PUNTO 1
+while True:
+    Z = input("Indicare la scelta:")
+    if Z == "1":
 
-somma = 0  # variabile per aggiungere somma interi
+        somma = 0  # variabile per aggiungere somma interi
 
-n = int(input("inserire numeri interi, 0 per chiudere: "))  # n da input
-if n !=0:  # se diverso da 0 si somma alla variabile
-    somma+=n
-while n != 0:   # finché diverso da 0 si ripete 
-    n = int(input("inserire numeri interi, 0 per chiudere: "))
-    if n!=0:
-        somma+=n
+        n = int(input("inserire numeri interi, 0 per chiudere: "))  # n da input
+        if n !=0:  # se diverso da 0 si somma alla variabile
+            somma+=n
+        while n != 0:   # finché diverso da 0 si ripete 
+            n = int(input("inserire numeri interi, 0 per chiudere: "))
+            if n!=0:
+                somma+=n
 
 
-print("La somma dei numeri è:", somma)        # stampa del risultato.
+        print("La somma dei numeri è:", somma)        # stampa del risultato.
 
 #---------------------------------------------
-
+        
 # PUNTO 2
 
-stringa = input("Inserire una stringa: ")   # stringa da input
+    elif Z == "2":
 
-for i in range(len(stringa)):  # scan della stringa con il len della string
-    print(stringa[i])  # stampa di ciascun carattere
+        stringa = input("Inserire una stringa: ")   # stringa da input
 
+        for i in range(len(stringa)):  # scan della stringa con il len della string
+            print(stringa[i])  # stampa di ciascun carattere
 
 #----------------------------------------------
 
 # PUNTO 3
+    elif Z == "3":
+        N = int(input("Indicare massimo numero range: "))
+        S = int(input("indicare lo step per il range: "))
 
-N = int(input("Indicare massimo numero range: "))
-S = int(input("indicare lo step per il range: "))
+        for i in range(0,N,S):
+            print(i)
 
-for i in range(0,N,S):
-    print(i)
+    else:
+        break
