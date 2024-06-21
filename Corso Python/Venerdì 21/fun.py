586484f2f116a9a3710a904e9e574dd767bb0216
DaniFila <150461747+DaniFila@users.noreply.github.com>
2024-06-21 17:25:09 +0200

    Add files via upload

---
+++ Corso Python/Venerdì 21/fun.py
@@ -0,0 +1,31 @@
+def ciao(nome):
+    if nome != "mirko":
+        print("ciao")
+
+
+
+def ciao2(nome):
+    print(nome)
+
+
+# x = input()
+
+ # ciao2(x)
+
+
+x = input()
+
+ # ciao2(x)
+
+
+
+
+ # ciao("antonio")
+
+
+def saluta(nome):
+    print("Ciao,", nome)
+    print("Benvenuto")
+
+
+saluta(x)
