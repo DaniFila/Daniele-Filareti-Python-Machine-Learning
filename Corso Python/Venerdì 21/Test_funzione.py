42feae52c504771f1f253b6535d81eba70d61336
DaniFila <150461747+DaniFila@users.noreply.github.com>
2024-06-21 15:54:26 +0200

    Add files via upload

--- Corso Python/Venerdì 21/Test_funzione.py
+++ Corso Python/Venerdì 21/Test_funzione.py
@@ -1,23 +1,23 @@
-def verifica_primo(n):
-    primo = True
+def verifica_primo(n):    # funzione per verifica un numero primo
+    primo = True        # variabile locale per verifica primo
     for i in range(2,n-1):
-        if n%i == 0:
+        if n%i == 0:       # scan per identificare divisori di n
             primo = False
     if primo:
-        return print("è un numero primo")
+        return print("è un numero primo")    # la funzione restituisce una stampa in base al valore di primo
     else:
         return print("non è un numero primo")
 
-def verifica_intero(a):
+def verifica_intero(a):    # funzione per verificare che la stringa digitata sia solo intero
     if not a.isdigit():
-        return False
+        return False    # restituisce valore booleano a seconda della condizione
     return True
 
 
 
-N = input("Inserire un numero per la verifica: ")
-while not verifica_intero(N):
+N = input("Inserire un numero per la verifica: ")   # numero preso in input
+while not verifica_intero(N):     # si verifica se il numero è inserito correttamente ed eventualmente si cicla richiamando la funzione 
     print("Errore")
-    N = input("Inserire un numero per la verifica: ")
+    N = input("Inserire un numero per la verifica: ") 
 
-verifica_primo(int(N))
+verifica_primo(int(N))   # infine si richiama la funzione per la verifica primo
