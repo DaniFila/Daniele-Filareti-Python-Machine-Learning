2bfeb7b75714296462ed07e5da57616ce4b6716b
DaniFila <150461747+DaniFila@users.noreply.github.com>
2024-06-24 16:18:28 +0200

    Add files via upload

---
+++ Corso Python/Lunedì 24/Es_4.py
@@ -0,0 +1,15 @@
+l =[]
+
+while True:
+    a = int(input("Inserire un numero (0 per terminare): "))
+    if a == 0:
+        break
+    else:
+        l.append(a)
+
+
+for i in l:
+    for j in range(i):
+        print("*",end="")
+    print("")    
+        
