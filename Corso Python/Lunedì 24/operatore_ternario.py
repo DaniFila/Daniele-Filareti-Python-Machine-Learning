db0da80fc33e58b259331552242f081a595148be
DaniFila <150461747+DaniFila@users.noreply.github.com>
2024-06-24 12:46:24 +0200

    Add files via upload

---
+++ Corso Python/Lunedì 24/operatore_ternario.py
@@ -0,0 +1,14 @@
+if 5 == 5: print("uguale")
+else: print("diverso")
+
+
+# OPERATORE TERNARIO
+
+var = print("uguale") if 5 != 5 else print("diverso")
+
+
+
+variabile = "uguale" if var == 5 else "diverso"
+
+
+print(variabile)
