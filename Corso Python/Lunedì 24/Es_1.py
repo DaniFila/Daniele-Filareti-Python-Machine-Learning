cd05535e984234b1abd6badd7e54dacf372af36d
DaniFila <150461747+DaniFila@users.noreply.github.com>
2024-06-24 11:57:13 +0200

    Add files via upload

---
+++ Corso Python/Lunedì 24/Es_1.py
@@ -0,0 +1,12 @@
+a = input("Inserire il tuo nome: ")
+b = int(input("Inserire il tuo anno di nascita: "))
+c = int(input("Inserire giorno della settimana: "))
+età = 0
+for i in range(b,2024):
+    età+=1
+
+weekend = 0
+for i in range(c,6):
+    weekend+=1
+print("Il tuo none è", a,"hai",età,"e mancano",weekend,"al weekend")
+
