6563dec98a7ac37835ca57b64c6ef10bb35124e0
DaniFila <150461747+DaniFila@users.noreply.github.com>
2024-07-01 17:14:33 +0200

    Add files via upload

--- Corso Python/Lunedì 1/Es4_class.py
+++ Corso Python/Lunedì 1/Es4_class.py
@@ -1,38 +1,38 @@
 class Ristorante:  
-    menu = {}    
-    apertura = False
-    def __init__(self,nome,tipo_cucina):
+    menu = {}    # dizionario per inserire pietanze con relativo prezzo
+    apertura = False  # booleano per verificare apertura/chiusura ristorante
+    def __init__(self,nome,tipo_cucina):  # parametri per creare gli oggetti
         self.nome = nome
         self.tipo_cucina = tipo_cucina
-    def stato_apertura(self):
+    def stato_apertura(self):  # funzione per verificare stato apertura attraverso booleano apertura
         if self.apertura:
             print("Aperto")
         else:
             print("Chiuso")
-    def apri_ristorante(self):
+    def apri_ristorante(self): # funzione per aprire ristorante e impostare booleano a True
         self.apertura = True
         print("Il ristorante è ora aperto!")
-    def chiudi_ristorante(self):
+    def chiudi_ristorante(self): # funzione per chiudere ristorante e impostare booleano a False
         self.apertura = False
         print("Il ristorante è ora chiuso!")
-    def aggiungi_menu(self):
+    def aggiungi_menu(self):   # funzione per aggiungere pietanza al dizionario menu con il prezzo
         cibo = input("Indicare pietanza: ")
         prezzo = input("Indicare il prezzo: ")
         self.menu[cibo] = prezzo
-    def togli_menu(self):
+    def togli_menu(self):  # funzione per eliminare pietanza dal menu dizionario
         cibo = input("indicare pietanza da eliminare: ")
         if cibo in self.menu:
             self.menu.pop(cibo)
             print("Eliminato con successo")
         else:
             print("Cibo non trovato")
-    def stampa_menu(self):
+    def stampa_menu(self):  # funzione per stampare menu se non è vuoto con verifica e avviso eventuale
         if self.menu == {}:
             print("Il menu è vuoto")
         else:    
             for i in self.menu:
                 print(i,"Prezzo:",self.menu[i])
-    def descrivi_ristorante(self):
+    def descrivi_ristorante(self):  # funzione che descrive il ristorante indicando il nome e la tipologia di cucina
         print(f"Questo ristorante si chiama {self.nome} e la tipologia della cucina è: {self.tipo_cucina}")
 
 
