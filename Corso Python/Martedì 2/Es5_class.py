387d0ec71560858c2024e3a8d702f676316ea586
DaniFila <150461747+DaniFila@users.noreply.github.com>
2024-07-02 16:55:42 +0200

    Add files via upload

---
+++ Corso Python/Martedì 2/Es5_class.py
@@ -0,0 +1,56 @@
+class Animale:
+    def __init__(self,nome,eta):
+        self.nome = nome
+        self.eta = eta
+ 
+    
+
+
+
+class Leone(Animale):
+    cacciatore = False
+    def __init__(self,nome,eta):
+        Animale.__init__(self,nome,eta)
+    def caccia(self):
+        if self.cacciatore:
+            print(f"Il leone di nome {self.nome} sta cacciando")
+        else:
+            print(f"Il leone di nome {self.nome} non sta cacciando")
+            self.cacciatore = True
+    def stato_caccia(self):
+        if self.cacciatore:
+            self.cacciatore = False
+        else:
+            self.cacciatore = True
+        print("Stato cambiato")
+    def fai_suono(self):
+        print(f"L'animale {self.nome} con età di {self.eta} fa un suono grgrgr")
+    
+
+class Giraffa(Animale):
+    def __init__(self,nome,eta,altezza):
+        self.alt = altezza
+        Animale.__init__(self,nome,eta)
+    def altezza(self):
+        print(f"L'altezza della giraffa è: {self.alt}")
+    def fai_suono(self):
+        print(f"L'animale {self.nome} con età di {self.eta} fa un suono auauauau")
+    
+
+class Pinguino(Animale):
+    def __init__(self,nome,eta,carattere):
+        self.carattere = carattere
+        Animale.__init__(self,nome,eta)
+    def carattere(self):
+        print(f"Il carattere del pinguino è: {self.carattere}")
+    def fai_suono(self):
+        print(f"L'animale {self.nome} con età di {self.eta} fa un suono tutututu")
+    
+
+
+
+
+lion = Giraffa("leone",55,25)
+
+lion.altezza()
+        
