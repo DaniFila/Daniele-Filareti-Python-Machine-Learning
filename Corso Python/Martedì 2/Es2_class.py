16aed0beff34481525b4622f3f49c355ae1bef0c
DaniFila <150461747+DaniFila@users.noreply.github.com>
2024-07-02 11:28:01 +0200

    Add files via upload

--- Corso Python/Martedì 2/Es2_class.py
+++ Corso Python/Martedì 2/Es2_class.py
@@ -31,45 +31,45 @@ class Abbigliamento:
 
 
 class Fabbrica:
-    def __init__(self):
-        self.dizionario = {"Elettronica":0,"Abbigliamento":0}
+    def __init__(self): 
+        self.dizionario = {"Elettronica":0,"Abbigliamento":0}  #attributi dell'istanza
         self.prodotti_in_fabbrica = []
         self.tipologia_prodotti = []
-    def aggiungi_prodotto(self):
+    def aggiungi_prodotto(self): # metodo per aggiungere prodotto
         nome = input("Indicare nome prodotto: ")
         costo_produzione = int(input("Indicare costo_produzione "))
         prezzo_vendita = int(input("Indicare prezzo di vendita "))
-        prod = Prodotto(nome,costo_produzione,prezzo_vendita)
+        prod = Prodotto(nome,costo_produzione,prezzo_vendita)  # mi crea l'oggetto prodotto
         while True:
-            tipo = input("Indicare tipologia prodotto\n1: elettronica\n2:abbigliamento\n")
+            tipo = input("Indicare tipologia prodotto\n1: elettronica\n2:abbigliamento\n") # si chiede la tipologia del prodotto
             if tipo == "1":
-                self.prodotti_in_fabbrica.append(prod)
-                self.tipologia_prodotti.append(Elettronica(prod))
-                self.dizionario["Elettronica"] += 1
+                self.prodotti_in_fabbrica.append(prod)  # si aggiunge in lista prodotti_in_fabbrica il prodotto
+                self.tipologia_prodotti.append(Elettronica(prod)) # si aggiunge in lista tipologia_prodotti l'oggetto prodotto con la tipologia 
+                self.dizionario["Elettronica"] += 1 # si aggiorna al dizionario il contatore
                 print("Aggiunto con successo")
                 break
             elif tipo == "2":
-                self.prodotti_in_fabbrica.append(prod)
-                self.tipologia_prodotti.append(Abbigliamento(prod))
-                self.dizionario["Abbigliamento"] += 1
+                self.prodotti_in_fabbrica.append(prod) # si aggiunge in lista prodotti_in_fabbrica il prodotto
+                self.tipologia_prodotti.append(Abbigliamento(prod)) # si aggiunge in lista tipologia_prodotti l'oggetto prodotto con la tipologia 
+                self.dizionario["Abbigliamento"] += 1 # si aggiorna al dizionario il contatore
                 print("Aggiunto con successo")
                 break
             else:
-                print("Scelta non valida")   
-    def vendi_prodotti(self):
-        a = input("Indicare nome prodotto: ")
-        for i in self.prodotti_in_fabbrica:
-            if i.nome == a:
-                print(i.calcola_profitto())
-                for j in self.tipologia_prodotti:
-                    if i == j.prodotto:
-                        if type(j) == Abbigliamento:
-                            self.tipologia_prodotti.remove(j)
-                            self.dizionario["Abbigliamento"] -=1
+                print("Scelta non valida")    # se non si digita 1 oppure 2 si ripete
+    def vendi_prodotti(self): # metodo per vendere prodotto
+        a = input("Indicare nome prodotto: ") # si indica il prodotto
+        for i in self.prodotti_in_fabbrica: 
+            if i.nome == a: # se il nome corrisponde al nome di un oggetto prodotto nella lista
+                print(i.calcola_profitto()) # si stampa il profitto
+                for j in self.tipologia_prodotti: 
+                    if i == j.prodotto: # se l'oggetto prodotto corrisponde all'oggetto prodotto della tipologia 
+                        if type(j) == Abbigliamento: # se è tipo abbigliamento
+                            self.tipologia_prodotti.remove(j) # si rimuove dalla lista tipologia
+                            self.dizionario["Abbigliamento"] -=1 # si aggiorna il dizionario
                         else:
-                            self.tipologia_prodotti.remove(j)
-                            self.dizionario["Elettronica"] -= 1
-                self.prodotti_in_fabbrica.remove(i)
+                            self.tipologia_prodotti.remove(j) # sennò si rimuove dalla lista tipologia
+                            self.dizionario["Elettronica"] -= 1 # si aggiorna il dizionario
+                self.prodotti_in_fabbrica.remove(i) # si rimuove il prodotto dalla lista prodotti
             else:
                 print("prodotto non disponibile")
     # def resi_prodotto(self):
