fa0421fdf474074b25e6e2b8c2eee3e2078e989a
DaniFila <150461747+DaniFila@users.noreply.github.com>
2024-07-02 17:15:16 +0200

    Add files via upload

--- Corso Python/Martedì 2/Es5_class.py
+++ Corso Python/Martedì 2/Es5_class.py
@@ -1,4 +1,4 @@
-class Animale:
+class Animale: # classe padre
     def __init__(self,nome,eta):
         self.nome = nome
         self.eta = eta
@@ -7,44 +7,50 @@ class Animale:
 
 
 
-class Leone(Animale):
+class Leone(Animale): # classe figlia
     cacciatore = False
     def __init__(self,nome,eta):
-        Animale.__init__(self,nome,eta)
-    def caccia(self):
+        Animale.__init__(self,nome,eta) # eredita classe padre Animale
+    def caccia(self): # metodo per stampare lo stato del leone di caccia
         if self.cacciatore:
             print(f"Il leone di nome {self.nome} sta cacciando")
         else:
             print(f"Il leone di nome {self.nome} non sta cacciando")
             self.cacciatore = True
-    def stato_caccia(self):
+    def stato_caccia(self): # metodo per cambiare stato caccina
         if self.cacciatore:
             self.cacciatore = False
         else:
             self.cacciatore = True
         print("Stato cambiato")
-    def fai_suono(self):
+    def fai_suono(self): # metodo per stampare suono del leone
         print(f"L'animale {self.nome} con età di {self.eta} fa un suono grgrgr")
     
 
-class Giraffa(Animale):
+class Giraffa(Animale): #classe figlia
     def __init__(self,nome,eta,altezza):
         self.alt = altezza
-        Animale.__init__(self,nome,eta)
-    def altezza(self):
-        print(f"L'altezza della giraffa è: {self.alt}")
-    def fai_suono(self):
+        Animale.__init__(self,nome,eta) # eredita classe padre Animale
+    def altezza(self): # metodo che stampa l'altezza della giraffa
+        print(f"L'altezza della giraffa è: {self.alt}") 
+    def fai_suono(self): # metodo per stampare suono
         print(f"L'animale {self.nome} con età di {self.eta} fa un suono auauauau")
     
 
-class Pinguino(Animale):
-    def __init__(self,nome,eta,carattere):
+class Pinguino(Animale): # classe figlia
+    def __init__(self,nome,eta,carattere): 
         self.carattere = carattere
-        Animale.__init__(self,nome,eta)
-    def carattere(self):
+        Animale.__init__(self,nome,eta) # eredita classe padre Animale
+    def carattere(self): # metodo per stampare carattere del pinguino
         print(f"Il carattere del pinguino è: {self.carattere}")
-    def fai_suono(self):
+    def fai_suono(self): # metodo per stampare suono del pinguino
         print(f"L'animale {self.nome} con età di {self.eta} fa un suono tutututu")
     
 
 
+# test
+
+lion = Giraffa("leone",55,25)
+
+lion.altezza()
+        
