9036a413b5f51ddc0f7cd55253fbfa66211cd3b3
DaniFila <150461747+DaniFila@users.noreply.github.com>
2024-07-03 12:44:52 +0200

    Add files via upload

--- Corso Python/Mercoledì 3/Es2_class.py
+++ Corso Python/Mercoledì 3/Es2_class.py
@@ -1,36 +1,45 @@
-class UnitaMilitare:
+class UnitaMilitare: # classe Padre
     difesa = 0
-    calibrazione = 0 
+    calibrazione = 0  # attributi di classe per azioni tipo di unità
     esplorazione = 0
     rifornimento = {}
-    conduzione = 0
-    def __init__(self,nome,numero_soldati):
+    ricognizionamento = 0
+    def __init__(self,nome,numero_soldati): 
         self.nome = nome
         self.numero_soldati = numero_soldati
 
-    def muovi(self):
+    def muovi(self): # metodo di stampa
         print(f"L'unità militare {self.nome} si sta muovendo...")
     
-    def attacca(self):
+    def attacca(self): # metodo di stampa
         print(f"L'unità militare {self.nome} con {self.numero_soldati} sta attaccando...")
 
-    def ritira(self):
-        pass
+    def ritira(self): # metodo di stampa
+        print("Elaborazione strategia...")
 
 
-class Fanteria(UnitaMilitare):
-    classe = "Fanteria"
+class Fanteria(UnitaMilitare): # classe figlia
+    classe = "Fanteria" # attributo con nome classe
     def __init__(self, nome, numero_soldati):
         UnitaMilitare.__init__(self,nome, numero_soldati)
-    def costruisci_trincea(self):
+    def costruisci_trincea(self): # metodo per costruire difesa
         print("E' stata costruita una trincea...")
         self.difesa +=1
+    def strat(self): # metodo che richiama metodo padre e aggiunge stampa
+        super().ritira()
+        print(f"La difesa attuale della fanteria è {self.difesa}")
 
-class Ricognizione(UnitaMilitare):
-    classe = "Ricognizione"
+class Ricognizione(UnitaMilitare): # classe figlia
+    classe = "Ricognizione" # attributo con nome classe
     def __init__(self, nome, numero_soldati):
         UnitaMilitare.__init__(self,nome, numero_soldati)
-
+    def conduci_ricognizione(self): # metodo per condurre ricognizione prendendo da input quantità e sommandola all'attributo della classe padre
+        a = int(input("indicare i metri per la ricognizione: "))
+        print(f"E' stata condotta una ricognizione di {a} metri...")
+        self.ricognizionamento += a
+    def strat(self): # metodo che stampa informazioni
+        super().ritira()
+        print(f"Sono stati effettuati {self.ricognizionamento} metri di ricognizione")
 
 
 
@@ -38,16 +47,7 @@ class SupportoLogistico(UnitaMilitare):
     classe = "SupportoLogistico"
     def __init__(self, nome, numero_soldati):
         UnitaMilitare.__init__(self,nome, numero_soldati)
-
-class Cavalleria(UnitaMilitare):
-    classe = "Cavalleria"
-    def __init__(self, nome, numero_soldati):
-        UnitaMilitare.__init__(self,nome, numero_soldati)
-    def esplora_terreno(self):
-        a = int(input("Quanto terreno vuoi esplorare? "))
-        print(f"Sono stati esplorati {a}Km quadrati di terreno...")
-        self.esplorazione += a
-    def rifornisci_unità(self):
+    def rifornisci_unità(self): # metodo che si rifornisce unità e si aggiunge nel dizionario della classe padre
         a = input("Indicare tipo di rifornimento: ")
         b = input("Indicare quantità")
         if  a in self.rifornimento:
@@ -55,14 +55,33 @@ class Cavalleria(UnitaMilitare):
         else:
             self.rifornimento[a] = b
         print("Rifornimento avvenuto con successo!")
+    def strat(self): # metodo che stampa informazioni
+        super().ritira()
+        print(f"Sono state rifornite le seguenti unità: {self.rifornimento}")
+
+class Cavalleria(UnitaMilitare):
+    classe = "Cavalleria"
+    def __init__(self, nome, numero_soldati):
+        UnitaMilitare.__init__(self,nome, numero_soldati)
+    def esplora_terreno(self): # metodo che esplora terreno e lo aggiunge all'attributo della classe padre
+        a = int(input("Quanto terreno vuoi esplorare? "))
+        print(f"Sono stati esplorati {a}Km quadrati di terreno...")
+        self.esplorazione += a
+    def strat(self):
+        super().ritira()
+        print(f"Sono stati esplorati {self.esplorazione} Km di terreno")
+
 
 class Artiglieria(UnitaMilitare):
     classe = "Artiglieria"
     def __init__(self, nome, numero_soldati):
         UnitaMilitare.__init__(self,nome, numero_soldati)
-    def calibra_artiglieria(self):
+    def calibra_artiglieria(self): # metodo per calibrare e aggiungere all'attributo della classe padre
         print("E' stata calibrata l'artiglieria...")
         self.calibrazione += 1
+    def strat(self):
+        super().ritira()
+        print(f"La calibrazione attuale è di {self.calibrazione}")
 
 
 class ControlloMilitare: # classe per controllo militare
