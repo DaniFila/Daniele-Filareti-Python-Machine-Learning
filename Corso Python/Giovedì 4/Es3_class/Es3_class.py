1487038f58fb95ee6cfcf80e955910e42e055327
DaniFila <daniele.filareti@icloud.com>
2024-07-04 12:25:23 +0200

    Update Es3_class.py

--- Corso Python/Giovedì 4/Es3_class/Es3_class.py
+++ Corso Python/Giovedì 4/Es3_class/Es3_class.py
@@ -1,7 +1,18 @@
 import modV
 
 
-v = modV.GestoreParcoVeicoli()
-a = modV.Veicolo()
+gestione_veicoli = modV.GestoreParcoVeicoli()
+veicolo1 = modV.Veicolo()
 
-v.gestore(a)
+# gestione_veicoli.gestore(veicolo1)
+
+veicolo2 = modV.Veicolo()
+veicolo3 = modV.Veicolo()
+
+# gestione_veicoli.aggiungi_veicolo(veicolo2)
+
+# gestione_veicoli.aggiungi_veicolo(veicolo3)
+
+# gestione_veicoli.rimuovi_veicolo("Lamborghini","Urus")
+gestione_veicoli.rimuovi_veicolo("ferrari","roma")
+gestione_veicoli.lista_veicoli()
