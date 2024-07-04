e482715d5adb17acdb01a5439d49728697c9b99b
DaniFila <daniele.filareti@icloud.com>
2024-07-04 15:40:34 +0200

    Update Es4_class.py

--- Corso Python/Giovedì 4/Es4_class.py
+++ Corso Python/Giovedì 4/Es4_class.py
@@ -2,7 +2,6 @@ class MetodoPagamento:
     def effettua_pagamento(self,importo):
         print("Elaborazione...")
 
-
 class CartaDiCredito(MetodoPagamento):
     def effettua_pagamento(self, importo):
         super().effettua_pagamento(importo)
@@ -13,25 +12,39 @@ class PayPal(MetodoPagamento):
         super().effettua_pagamento(importo)
         print(f"Pagamento di {importo} eseguito correttamente!")
 
-
 class BonificoBancario(MetodoPagamento):
     def effettua_pagamento(self, importo):
         super().effettua_pagamento(importo)
         iban = input("Inserisci iban: ")
         print(f"Pagamento tramite Bonifico di {importo} su iban: {iban},eseguito correttamente!")
 
-
 class GestorePagamenti:
-    def paga(tipo_pagamento,importo):
+    def paga(self,tipo_pagamento,importo):
         try:
             tipo_pagamento.effettua_pagamento(importo)
             print("Successo!")
         except:
             print("Tipo di pagamento non valido!")
 
+class Menu():
+    def pagamento(self):
+        while True:
+            a = input("Indicare metodo di pagamento:\n1: Carta di Credito\n2: PayPal\n3: Bonifico Bancario\n")
+            if a == "1":
+                z = CartaDiCredito()
+                break
+            elif a == "2":
+                z = PayPal()
+                break
+            elif a == "3":
+                z = BonificoBancario()
+                break
+            else:
+                print("Error")
+        importo = int(input("Indicare importo: "))
+        gestione = GestorePagamenti()
+        gestione.paga(z,importo)
+            
 
-p = CartaDiCredito()
-
-d = GestorePagamenti
-
-d.paga(p,20)
+a = Menu()
+a.pagamento()
