e149fec3c35e0557e9ff46bf97576de0ea01f1b0
DaniFila <daniele.filareti@icloud.com>
2024-07-04 15:27:46 +0200

    Create Es4_class.py

---
+++ Corso Python/Giovedì 4/Es4_class.py
@@ -0,0 +1,33 @@
+class MetodoPagamento:
+    def effettua_pagamento(self,importo):
+        print("Elaborazione...")
+
+
+class CartaDiCredito(MetodoPagamento):
+    def effettua_pagamento(self, importo):
+        super().effettua_pagamento(importo)
+        print("Transazione eseguita con Carta di Credito!")
+    
+class PayPal(MetodoPagamento):
+    def effettua_pagamento(self, importo):
+        super().effettua_pagamento(importo)
+        print(f"Pagamento di {importo} eseguito correttamente!")
+
+
+class BonificoBancario(MetodoPagamento):
+    def effettua_pagamento(self, importo):
+        super().effettua_pagamento(importo)
+        iban = input("Inserisci iban: ")
+        print(f"Pagamento tramite Bonifico di {importo} su iban: {iban},eseguito correttamente!")
+
+
+class GestorePagamenti:
+    def paga(tipo_pagamento,importo):
+        tipo_pagamento.effettua_pagamento(importo)
+
+
+p = CartaDiCredito()
+
+d = GestorePagamenti
+
+d.paga(p,20)
