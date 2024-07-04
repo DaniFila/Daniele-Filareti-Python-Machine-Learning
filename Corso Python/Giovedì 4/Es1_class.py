9e45a4d7a47380d372b4b800c41b1129052493e8
DaniFila <daniele.filareti@icloud.com>
2024-07-04 09:51:51 +0200

    Update Es1_class.py

--- Corso Python/Giovedì 4/Es1_class.py
+++ Corso Python/Giovedì 4/Es1_class.py
@@ -1,32 +1,32 @@
 class ContoBancario:
     def __init__(self):
-        self.__titolare = ""
-        self.__saldo = 0
-    def deposita(self,importo):
+        self.__titolare = "" # attributo privato __
+        self.__saldo = 0 # attributo privato __
+    def deposita(self,importo): # metodo per depositare importo dato nel metodo
         if importo > 0:
             self.__saldo += importo
         else:
             print("Importo non valido")
-    def preleva(self,importo):
+    def preleva(self,importo): # metodo per prelevare importo indicato nell'inserimento del metodo
         if self.__saldo >= importo:
             self.__saldo -= importo
             print("Prelievo completato")
         else:
             print("Saldo non disponibile")
-    def visualizza_saldo(self):
+    def visualizza_saldo(self): # metodo per visualizzare attributo privato saldo
         return self.__saldo
     
-    def set_titolare(self):
+    def set_titolare(self): # metodo per impostare nome titolare
         a = input("Inserire nome del titolare: ")
         if a != "":
             self.__titolare = a
             print("Nome impostato correttamente!")
         else:
             print("Non valido")
-    def get_titolare(self):
+    def get_titolare(self): # metodo per avere il nome del titolare
         return self.__titolare
     
-    def set_saldo(self):
+    def set_saldo(self): # metodo per impostare il saldo
         a = int(input("Inserire saldo: "))
         if a >0:
             self.__saldo = a
