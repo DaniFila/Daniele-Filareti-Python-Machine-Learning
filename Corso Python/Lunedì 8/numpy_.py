30ddcaeb54588f18e4265150ca6d065756c64331
DaniFila <daniele.filareti@icloud.com>
2024-07-08 15:36:34 +0200

    Create numpy_.py

---
+++ Corso Python/Lunedì 8/numpy_.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+# creazione di un array unidimensionale
+arr = np.array([1,2,3,4,5])
+
+
+# creazione di un array bidimensionale
+
+b = np.array([[1,2,3],[4,5,6]])
+
+
+print(arr)
+print(b)
+
+
+
