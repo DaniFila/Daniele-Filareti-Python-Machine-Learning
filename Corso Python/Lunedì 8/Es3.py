be5c1836b386272b586759879c0ec5ce144d99bc
DaniFila <daniele.filareti@icloud.com>
2024-07-08 16:27:53 +0200

    Update Es3.py

---
+++ Corso Python/Lunedì 8/Es3.py
@@ -0,0 +1,28 @@
+import numpy as np
+# Esercizio 1
+
+arr = np.full(10,5) # creato array di 10 elementi uguali a 5
+
+arr_matr = arr.reshape(2,5) # dall'array si crea una matrice
+
+matr_casuale = np.random.rand(4,4) # matrice casuale 4x4 con numeri tra 0 e 1
+
+#-------------------------------------------------------------------------------------------
+
+
+arr1 = np.array([[1,2,3],[4,5,6],[7,8,9]]) # matrice 1 3x3
+
+arr2 = np.array([[1,2,5],[46,5,6],[7,8,10]]) # matrice 2 3x3
+
+for i in range(3):
+    for j in range(3):
+        prodotto = arr1[i][j]*arr2[i][j] # prodotto delle 2 matrici elemento per elemento
+        # print(prodotto)
+
+
+arr3 = np.linspace((1,9),3,3)
+
+somma = np.sum(arr3)
+
+
+
