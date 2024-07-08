4d1884db59469500290b877e30ca601f1f8468d8
DaniFila <daniele.filareti@icloud.com>
2024-07-08 16:11:39 +0200

    Update Es1.py

--- Corso Python/Lunedì 8/Es1.py
+++ Corso Python/Lunedì 8/Es1.py
@@ -9,10 +9,9 @@ print(f"Il formato iniziale: {arr.dtype}")
 
 arr = arr.astype(np.float64)
 
-print(f"Il formato iniziale: {arr.dtype}")
+print(f"Il formato cambiato: {arr.dtype}")
 
 
-print(arr.dtype)
 
 
 print(f"La forma dell'array è la seguente:\n{arr}")
