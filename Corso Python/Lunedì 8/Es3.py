a5b870a464a2939810ac86fbbb80fcf673542e75
DaniFila <daniele.filareti@icloud.com>
2024-07-08 16:38:10 +0200

    Update Es3.py

--- Corso Python/Lunedì 8/Es3.py
+++ Corso Python/Lunedì 8/Es3.py
@@ -7,8 +7,8 @@ arr_matr = arr.reshape(2,5) # dall'array si crea una matrice
 
 matr_casuale = np.random.rand(4,4) # matrice casuale 4x4 con numeri tra 0 e 1
 
-#-------------------------------------------------------------------------------------------
-
+#--------------------------------------------------------------------------------------------
+# Esercizio 2
 
 arr1 = np.array([[1,2,3],[4,5,6],[7,8,9]]) # matrice 1 3x3
 
@@ -23,6 +23,27 @@ for i in range(3):
 arr3 = np.linspace((1,9),3,3)
 
 somma = np.sum(arr3)
+
+#--------------------------------------------------------------------------------------------
+# Esercizio 3
+
+def crea_array_equi():
+    n = int(input("Indicare numero di elementi: "))
+    inizio = int(input("Indicare estremo sinistro: "))
+    fine = int(input("indicare estremo destro: "))
+    arr = np.linspace(inizio,fine,n)
+    return arr
+
+def matr_random():
+    arr = np.random.randint(1,101, size=(5,5))
+    return arr
+
+
+#----------------------------------------------------------------------------------------------
+
+# Esercizio 4
+
+
 
 
 
