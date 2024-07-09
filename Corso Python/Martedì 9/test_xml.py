750c30419fc896ac69e7473683f10a1fa7bed427
DaniFila <daniele.filareti@icloud.com>
2024-07-09 10:17:17 +0200

    Create test_xml.py

---
+++ Corso Python/Martedì 9/test_xml.py
@@ -0,0 +1,34 @@
+import xml.etree.ElementTree as ET
+
+def xml_to_file():
+    tree = ET.parse("Corso Python/Martedì 9/path_to_file.xml")
+
+    root = tree.getroot()
+
+    return tree,root
+
+def xml_to_string():
+    xml_data = '''<?xml version="1.0"?>
+    <saluti>
+    <saluto>Hello World</saluto>
+    </saluti>'''
+
+    root = ET.fromstring(xml_data)
+
+    return root
+
+
+tree,root = xml_to_file()
+
+# root = xml_to_string()
+
+#for saluto in root.findall('saluto'):
+    #print(saluto.text)
+
+new_element = ET.Element('nuovo_tag')
+
+new_element.text = "testo del nuovo elemento"
+
+root.append(new_element)
+
+tree.write("Corso Python/Martedì 9/file.xml")
