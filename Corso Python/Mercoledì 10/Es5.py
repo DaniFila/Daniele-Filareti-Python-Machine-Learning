0aa080db3f9d232f49027ab92f1f4df59a3b0a3a
DaniFila <daniele.filareti@icloud.com>
2024-07-11 09:52:11 +0200

    Update Es5.py

--- Corso Python/Mercoledì 10/Es5.py
+++ Corso Python/Mercoledì 10/Es5.py
@@ -4,11 +4,11 @@ from random import randint
 
 def scrivi_file(dizionario):
     file_json= json.dumps(dizionario)
-    with open("4 settimana\mercoledi 5\pokedex.json","w") as file:
+    with open("Corso Python/Mercoledì 10/Es5.json","w") as file:
         file.write(file_json)
 
 def lettura_file():
-    with open("4 settimana\mercoledi 5\pokedex.json", "r") as file:
+    with open("Corso Python/Mercoledì 10/Es5.json", "r") as file:
         contenuto = file.read()
 
     dizionario_json = json.loads(contenuto)
@@ -26,20 +26,47 @@ def verifica_file():
 
 def pokemon_casuale():
     casuale=str(randint(1,20))
-    risposta=requests.get(f"https://pokeapi.co/api/v2/pokemon/1")
-    risposta_json=risposta.json()
-    diz_temp={}
-    exp=risposta_json["base_experience"]
-    print(exp)
-    nome=risposta_json["forms"][0]["name"]
-    print(nome)
-    id=risposta_json["id"]
-    print(id)
-    abilita=risposta_json["abilities"]
-    peso=risposta_json["weight"]
-    altezza=risposta_json["height"]
+    risposta=requests.get(f"https://pokeapi.co/api/v2/pokemon/{casuale}")
+    api_poke_casuale=risposta.json()
+    pokemon={}
+    exp=api_poke_casuale["base_experience"]
+    nome=api_poke_casuale["forms"][0]["name"]
+    id=api_poke_casuale["id"]
+    abilita=api_poke_casuale["abilities"]
+    peso=api_poke_casuale["weight"]
+    altezza=api_poke_casuale["height"]
+    pokemon = {"nome":nome,"exp":exp,"abilità":abilita,"peso":peso,"altezza":altezza}
+    
+    return pokemon,id
 
-    return diz_temp
+def aggiornamento_pokedex(pokemon,pokedex,id):
+    trovato = False
+    for idp in pokedex:
+        if idp == id:
+            trovato = True
+            print("Il pokemon si trova già nel pokedex")
+    if not trovato:
+        print("Il pokemon non si trova nel pokedex,aggiunta in corso...")
+        pokedex[id] = pokemon
+        print("Pokemon aggiunto!")
+        scrivi_file(pokedex)
 
-pokedex=verifica_file()
-pokemon_casuale()
+def visualizza_pokedex(pokedex):
+    for chiave in pokedex:
+        id = chiave
+        print(f"ID: {id} nome: {pokedex[id]["nome"]},exp: {pokedex[id]["exp"]}, abilità: {pokedex[id]["abilità"]}, peso: {pokedex[id]["peso"]}, altezza: {pokedex[id]["altezza"]}")
+    
+
+
+
+
+
+
+
+
+
+
+pokedex = verifica_file()
+pokemon, id = pokemon_casuale()
+#aggiornamento_pokedex(pokemon,pokedex,id)
+visualizza_pokedex(pokedex)
