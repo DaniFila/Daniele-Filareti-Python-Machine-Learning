90136cbe4c74c824323a6871faeca295cec2f095
DaniFila <daniele.filareti@icloud.com>
2024-07-12 14:54:02 +0200

    Update db.py

--- Corso Python/Venerdì 12/db.py
+++ Corso Python/Venerdì 12/db.py
@@ -1,13 +1,40 @@
+"""#query = "create database testpython"
+query = "SHOW DATABASES"
+my_cursor.execute(query)
+
+for database in my_cursor:
+print(database)"""
+
+
+
 import mysql.connector
 
-try:
-    mydb = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "root",
-    port = "8889"
-    )
+def connessione_server():
+    try:
+        mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "root",
+        port = "8889"
+        )
+        print("Connessione avvenuta!")
+    except:
+        print("Errore")
 
-    print("Connessione avvenuta!")
-except:
-    print("Errore")
+def connessione_database():
+    try:
+        mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "root",
+        port = "8889",
+        database = "testpython"
+        )
+        my_cursor = mydb.cursor()
+        print("Connessione avvenuta!")
+        return my_cursor
+    except:
+        print("Errore")
+
+
+my_cursor = connessione_database()
