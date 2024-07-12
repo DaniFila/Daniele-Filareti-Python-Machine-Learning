4abbf118d6c57980c2cceb6644210fca7bf6dc0a
DaniFila <daniele.filareti@icloud.com>
2024-07-12 15:12:10 +0200

    Update db.py

--- Corso Python/Venerdì 12/db.py
+++ Corso Python/Venerdì 12/db.py
@@ -40,6 +40,29 @@ def connessione_database():
 mydb = connessione_database()
 my_cursor = mydb.cursor()
 
-query = "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))"
+#query = "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))"
+#query = "show tables"
 
-my_cursor.execute(query)
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = [
+  ('Peter', 'Lowstreet 4'),
+  ('Amy', 'Apple st 652'),
+  ('Hannah', 'Mountain 21'),
+  ('Michael', 'Valley 345'),
+  ('Sandy', 'Ocean blvd 2'),
+  ('Betty', 'Green Grass 1'),
+  ('Richard', 'Sky st 331'),
+  ('Susan', 'One way 98'),
+  ('Vicky', 'Yellow Garden 2'),
+  ('Ben', 'Park Lane 38'),
+  ('William', 'Central st 954'),
+  ('Chuck', 'Main Road 989'),
+  ('Viola', 'Sideway 1633')]
+
+my_cursor.executemany(sql, val)
+
+mydb.commit()
+
+print(my_cursor.rowcount, "record inserted.")
+
+
