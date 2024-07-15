577fbbf13cb8919fda2d219f23ecd2b3e73035d0
DaniFila <150461747+DaniFila@users.noreply.github.com>
2024-07-15 17:46:00 +0200

    Add files via upload

--- Corso Python/Lunedì 15/Es2.py
+++ Corso Python/Lunedì 15/Es2.py
@@ -4,16 +4,26 @@ def crea_array():
     return a
 
 def main():
-    arr = crea_array() # richiamo la funzione per creare array
-    print(f"Array di partenza: {arr}") # stampo array di partenza
-    print(f"I primi 10 elementi dell'array: {arr[:10]}") # stampo i primi 10 elementi dell'array
-    print(f"Gli ultimi 5 elementi dell'array: {arr[15:21]}") # stampo gli ultimi 5 elementi dell'array
-    print(f"Elementi dall'indice 5 all'indici 15 escluso: {arr[5:15]}") # stampo gli elementi dall'indici 5 al 15 escluso
-    print(f"Ogni terzo elemento dell'array: {arr[::3]}") # stampo ogni terzo elemento dell'array
-    print("Cambio elementi con valore 99 dall'indice 5 al 10 escluso...")
-    arr[5:10] = 99 # modifico il valore degli elementi dell'array nell'intervallo di indice 5 e 10 escluso
-    print(f"Risultato: {arr}") # stampo l'array modificato
-
-
+    while True:
+        arr = crea_array() # richiamo la funzione per creare array 
+        a = f"Array: {arr}" # stampo array 
+        b = f"I primi 10 elementi dell'array: {arr[:10]}" # stampo i primi 10 elementi dell'array
+        c = f"Gli ultimi 5 elementi dell'array: {arr[15:21]}" # stampo gli ultimi 5 elementi dell'array
+        d = f"Elementi dall'indice 5 all'indici 15 escluso: {arr[5:15]}" # stampo gli elementi dall'indici 5 al 15 escluso
+        e = f"Ogni terzo elemento dell'array: {arr[::3]}" # stampo ogni terzo elemento dell'array
+        f = arr[5:10] = 99 # modifico il valore degli elementi dell'array nell'intervallo di indice 5 e 10 escluso
+        arr = crea_array() # richiamo la funzione per creare array 
+        diz = {"1":b,"2":c,"3":d,"4":e,"5":f}
+        scelta_menu = menù()
+        print(diz[scelta_menu])
+        print(f"Array intero: {arr}")
+        z = input("Vuoi ripetere?")
+        if z != "si":
+            break
+        
+def menù():
+    info = "\n1: primi 10 elementi dell'array\n2: ultimi 5 elementi dell'array\n3:Elementi dall'indice 5 all'indici 15 escluso\n4: Ogni terzo elemento dell'array\n5: Cambio elementi con valore 99 dall'indice 5 al 10 escluso\n "
+    scelta_menu = input(info)
+    return scelta_menu
 
 main()
