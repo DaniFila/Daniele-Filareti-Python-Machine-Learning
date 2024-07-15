b9348d0791d31e2e21dd3a0b1c9af3ded0dbb864
DaniFila <150461747+DaniFila@users.noreply.github.com>
2024-07-15 16:38:27 +0200

    Add files via upload

--- Corso Python/Lunedì 15/numpy_2.py
+++ Corso Python/Lunedì 15/numpy_2.py
@@ -2,17 +2,17 @@ import numpy as np
 
 arr = np.array([1,2,3,4,5])
 # Indexing
-print(arr[0])
+print(arr[0]) # stampa l'indice 0 dell'array
 # Slicing
-print(arr[1:3])
+print(arr[1:3]) # stampa dall'indice 1 all'indice 3 escluso
 # Boolean Indexing
-print(arr[arr>2])
+print(arr[arr>2]) # stampa i numeri che sono maggiori di 2
 print()
 arr_2d = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
 # Slicing sulle righe
-print(arr_2d[1:3])
+print(arr_2d[1:3]) # stampa solo la seconda e terza riga
 # Slicing sulle colonne
-print(arr_2d[:,1:3])
+print(arr_2d[:,1:3]) # stampa di tutte le righe solo la seconda e terza colonna
 # Slicing misto
-print(arr_2d[1:,1:3])
+print(arr_2d[1:,1:3]) # stampa dalla seconda riga solo la seconda e terza colonna
 
