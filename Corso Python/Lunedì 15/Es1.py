2f3ad096a629d59dccf2c077f0280fa0c28bdaea
DaniFila <150461747+DaniFila@users.noreply.github.com>
2024-07-15 15:59:08 +0200

    Add files via upload

--- Corso Python/Lunedì 15/Es1.py
+++ Corso Python/Lunedì 15/Es1.py
@@ -1,11 +1,15 @@
 import numpy as np
 
-arr = np.arange(10,50)
 
 
-arr = arr.astype('float64')
-
-print(arr.dtype)
-
-print(arr)
-
+while True:
+    a = int(input("Indicare estremo sinistro: ")) # si indica estremo sinistro
+    b = int(input("Indicare estremo destro (escluso): ")) # si indica estremo destro non incluso
+    arr = np.arange(a,b) # si crea l'array nel range
+    print("Il tipo di dato dell'array è:",arr.dtype) # si stampa il tipo di dato
+    arr = arr.astype('float64') # si modifica il tipo in float64
+    print("Il tipo di dato modificato è:",arr.dtype) # si stampa il tipo di dato modificato
+    print("L'array è:",arr)  # si stampa l'array
+    z = input("Vuoi ripetere? ") # si richiede se si vuole ripetere
+    if z != "si":
+        break
