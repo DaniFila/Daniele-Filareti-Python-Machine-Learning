ded0eb46b11dc75279a262996418f0912a6505e4
DaniFila <daniele.filareti@icloud.com>
2024-07-16 10:25:01 +0200

    Update Es1.py

--- Corso Python/Martedì 16/Es1.py
+++ Corso Python/Martedì 16/Es1.py
@@ -21,18 +21,46 @@ def punto_5(in_matr):
     in_matr[in_matr%3==0] = -1 # modifico tutti i valori che sono multipli per 3 e li cambio con -1
     return in_matr
 
-def main(): # funzione main che richiama tutte le funzioni e stampa i vari risultati
-    matr = punto_1()
+def punto_6(matr,matr_4x4,in_matr,diag_princ,in_matr_5): # funzione che richiama tutte le funzioni e stampa i vari risultati
     print(f"Matrice originale:\n {matr}\n")
-    matr_4x4 = punto_2(matr)
     print(f"Sotto matrice 4x4:\n {matr_4x4}\n")
-    in_matr = punto_3(matr_4x4)
     print(f"Matrice invertita:\n {in_matr}\n")
-    diag_princ = punto_4(in_matr)
     print(f"Diagonale principale:\n {diag_princ}\n")
-    in_matr_5 = punto_5(in_matr)
     print(f"Matrice invertita modificata:\n {in_matr_5}\n")
 
+def menu():
+    info = """1: crea sotto matrice
+2: inverti righe matrice
+3: estrai diagonale principale
+4: modifica matrice invertita
+5: stampa tutte le funzioni
+6: exit
+"""
+    scelta_menu = input(info)
+    return scelta_menu
+
+def main():
+    matr = punto_1()
+    matr_4x4 = punto_2(matr)
+    in_matr = punto_3(matr_4x4)
+    diag_princ = punto_4(in_matr)
+    in_matr_5 = punto_5(in_matr)
+    while True:
+        scelta_menu = menu()
+        if scelta_menu == "1":
+            print(f"Sotto matrice 4x4:\n {matr_4x4}\n")
+        elif scelta_menu == "2":
+            print(f"Matrice invertita:\n {in_matr}\n")
+        elif scelta_menu == "3":
+            print(f"Diagonale principale:\n {diag_princ}\n")
+        elif scelta_menu == "4":
+            print(f"Matrice invertita modificata:\n {in_matr_5}\n")
+        elif scelta_menu == "5":
+            punto_6(matr,matr_4x4,in_matr,diag_princ,in_matr_5)
+        elif scelta_menu == "6":
+            break
+        else:
+            print("Scelta non valida")
 
 main()
 
