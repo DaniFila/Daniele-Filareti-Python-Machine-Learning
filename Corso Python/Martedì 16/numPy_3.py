e0e69a65abc2374307b67b52cc776fd9f001147b
DaniFila <daniele.filareti@icloud.com>
2024-07-16 14:59:00 +0200

    Update numPy_3.py

--- Corso Python/Martedì 16/numPy_3.py
+++ Corso Python/Martedì 16/numPy_3.py
@@ -1,7 +1,19 @@
 import numpy as np
-
+# Inversa di una matrice
 A = np.array([[1,2],[3,4]]) # creazione di una matrice quadrata
 
 A_inv = np.linalg.inv(A) # calcolo dell'inversa della matrice
 
 print(A,"\n",A_inv)
+
+
+
+# Norma di un vettore
+
+v = np.array([3,4])
+
+# calcolo della norma del vettore
+
+norm_v = np.linalg.norm(v)
+
+print(norm_v)
