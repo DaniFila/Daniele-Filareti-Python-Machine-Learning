c66f0f659f45a8a36e9f3094e8cbe2beee984bd8
DaniFila <daniele.filareti@icloud.com>
2024-07-16 10:15:08 +0200

    Update Es1.py

--- Corso Python/Martedì 16/Es1.py
+++ Corso Python/Martedì 16/Es1.py
@@ -1,31 +1,27 @@
 import numpy as np
 
 def punto_1():
-    matr = np.random.randint(1,101,36)
-    matr_s = matr.reshape(6,6)
+    matr = np.random.randint(1,101,36) # creo array con 36 elementi casuali tra 1 e 100
+    matr_s = matr.reshape(6,6) # riformatto l'array in matrice 6x6
     return matr_s
 
-
 def punto_2(matr):
-    matr_4x4 = matr[1:5,1:5]
+    matr_4x4 = matr[1:5,1:5] # estraggo la matrice centrale da quella 6x6 in una matrice 4x4 utilizzando lo slicing di righe e colonne
     return matr_4x4
 
-
 def punto_3(matr_4x4):
-    in_matr = matr_4x4[::-1]
+    in_matr = matr_4x4[::-1] # inverto le righe di posizione
     return in_matr
 
 def punto_4(in_matr):
-    diag_princ = in_matr.diagonal()
+    diag_princ = in_matr.diagonal() # estraggo la matrice diagonale principale da quella invertita
     return diag_princ
 
 def punto_5(in_matr):
-    c_in_matr = in_matr
-    c_in_matr[c_in_matr%3==0] = -1
-    return c_in_matr
+    in_matr[in_matr%3==0] = -1 # modifico tutti i valori che sono multipli per 3 e li cambio con -1
+    return in_matr
 
-
-def main():
+def main(): # funzione main che richiama tutte le funzioni e stampa i vari risultati
     matr = punto_1()
     print(f"Matrice originale:\n {matr}\n")
     matr_4x4 = punto_2(matr)
