a7dd2487bccc1b92437b696f2336f11f348b673a
DaniFila <daniele.filareti@icloud.com>
2024-07-16 17:46:53 +0200

    Update mod_Es7.py

--- Corso Python/Martedì 16/mod_Es7.py
+++ Corso Python/Martedì 16/mod_Es7.py
@@ -1,8 +1,7 @@
 import numpy as np
-
-def crea_matrice():
-    while True:
-        try:
+def crea_matrice(): #creazione della matrice
+    while True: 
+        try:        #inserimento righe e colonne
             righe=int(input("Inserisci il numero di righe: "))
             break
         except:
@@ -15,30 +14,31 @@ def crea_matrice():
             print("Errore. Riprova")
 
     matrice = np.random.randint(0,100,size=(righe,col))
-    print(matrice)
+    print(f"La matrice è:\n {matrice}")
+
     return matrice,righe,col
 
-def sotto_matrice(matrice,righe,col):
+def sotto_matrice(matrice,righe,col): #calcolo sottomatrice dalla prima riga alla penultima, e dalla prima colonna alla penultima
     print(f"Sotto matrice centrale:\n {matrice[1:righe-1,1:col-1]}")
 
-def trasp_matrice(matrice):
+def trasp_matrice(matrice): #calcolo matrice trasposta
     matr=matrice.T
     print(matr)
 
-def somma_elem(matrice):
+def somma_elem(matrice): #somma degli elementi della matrice
     print(f"La somma della matrice è: {np.sum(matrice)}")
 
-def det_mat(matrice,righe,col):
+def det_mat(matrice,righe,col): #calcolo determinante della matrice se righe==col
     if righe==col:
         print(f"Il determinante è: {np.linalg.det(matrice)}")
     else:
         print("Righe e colonne diverse. Non si può calcolare il determinante")
 
-def molt_elem_wise(mat,righe,col):
+def molt_elem_wise(mat,righe,col): #moltiplicazione element-wise
     mat2=np.random.randint(0,100,size=(righe,col))
     molt=mat*mat2
     print(f"Seconda matrice generata:\n{mat2}")
     print(f"Moltiplicazione delle due matrici:\n {molt}")
 
-def media_mat(matrice):
+def media_mat(matrice): #calcolo media della matrice
     print(f"La media della matrice è: {np.mean(matrice)}")
