25f6bce9993cddc7d736995f38b6a3921cd61a75
DaniFila <daniele.filareti@icloud.com>
2024-07-16 11:10:47 +0200

    Update Es2.py

--- Corso Python/Martedì 16/Es2.py
+++ Corso Python/Martedì 16/Es2.py
@@ -2,22 +2,22 @@ import numpy as np
 
 
 
-def crea_arr_equi():
+def crea_arr_equi(): # funzione che genera array con 12 elementi equidistanti da 0 e 1
     arr_equi = np.linspace(0,1,12)
     return arr_equi
-def trasfroma_arr_in_matr(arr_equi):
+def trasfroma_arr_in_matr(arr_equi): # funzione che trasforma array in matrice 3x4
     matr_arr_equi = arr_equi.reshape(3,4)
     return matr_arr_equi
-def crea_matr_random():
+def crea_matr_random(): # funzione che crea matrice 3x4 con numeri casuali tra 0 e 1
     matr_random = np.random.rand(3,4)
     return matr_random
 
 
-def somma(matr):
+def somma(matr): # funzione che calcola la somma di tutti gli elementi di una matrice
     sum = np.sum(matr)
     return sum
-
-def main():
+ 
+def main(): # funzione main che richiama tutte le funzioni e stampa la somma di tutti gli elementi delle 2 matrici
     arr_equi = crea_arr_equi()
     matr_arr_equi = trasfroma_arr_in_matr(arr_equi)
     matr_random = crea_matr_random()
