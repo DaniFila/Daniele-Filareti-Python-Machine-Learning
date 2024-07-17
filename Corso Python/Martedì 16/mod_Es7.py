eeb9025d31f830094701e5e487b53d4d33a84bf8
DaniFila <daniele.filareti@icloud.com>
2024-07-17 09:05:53 +0200

    Update mod_Es7.py

--- Corso Python/Martedì 16/mod_Es7.py
+++ Corso Python/Martedì 16/mod_Es7.py
@@ -1,44 +1,50 @@
-import numpy as np
-def crea_matrice(): #creazione della matrice
-    while True: 
-        try:        #inserimento righe e colonne
-            righe=int(input("Inserisci il numero di righe: "))
-            break
-        except:
-            print("Errore. Riprova")
+import mod_Es7 as mod1     #import modulo con funzioni
+
+#menu
+def menu():
+    print("""\nMenù:
+1. Stampa matrice
+2. Estrai sottomatrice
+3. Matrice trasposta        
+4. Somma elementi matrice
+5. Moltiplicazione element-wise
+6. Media elementi matrice
+7. Calcolo determinante                     
+0. Esci
+""")
+    scelta_menu=input("\nScegli un opzione: ")
+    return scelta_menu
+
+#main
+def main():
+    print("\nCreazione Matrice 2D...\n")
+    mat,righe,col=mod1.crea_matrice()   #creazione della matrice di partenza fuori dal while
+
     while True:
-        try:
-            col=int(input("Inserisci il numero di colonne: "))
+        scelta=menu()
+        if scelta=="1":         #Stampa matrice
+            print(f"\nLa matrice 2D di partenza è:\n {mat}")
+        elif scelta=="2":       #Estrai sottomatrice
+            mod1.sotto_matrice(mat,righe,col)
+        elif scelta=="3":       #Matrice trasposta
+            mod1.trasp_matrice(mat)
+        elif scelta=="4":       #Somma elementi matrice
+            mod1.somma_elem(mat)
+        elif scelta=="5":       #Moltiplicazione element-wise
+            mod1.molt_elem_wise(mat,righe,col)
+        elif scelta=="6":       #Media elementi matrice
+            mod1.media_mat(mat)
+        elif scelta=="7":       #Calcolo determinante
+            mod1.det_mat(mat,righe,col)
+        elif scelta=="0":       #Esci
             break
-        except:
-            print("Errore. Riprova")
+        else:
+            print("Scelta sbagliata")
 
-    matrice = np.random.randint(0,100,size=(righe,col))
-    print(f"La matrice è:\n {matrice}")
+        nuova_scelta = input("\nVuoi effettuare un'altra operazione? ") #ripetibilità gestita tramite la richiesta di una nuova operazione
+        if nuova_scelta.lower() != "si":
+            print("\nSistema in chiusura. Arrivederci!")
+            break
 
-    return matrice,righe,col
 
-def sotto_matrice(matrice,righe,col): #calcolo sottomatrice dalla prima riga alla penultima, e dalla prima colonna alla penultima
-    print(f"Sotto matrice centrale:\n {matrice[1:righe-1,1:col-1]}")
-
-def trasp_matrice(matrice): #calcolo matrice trasposta
-    matr=matrice.T
-    print(matr)
-
-def somma_elem(matrice): #somma degli elementi della matrice
-    print(f"La somma della matrice è: {np.sum(matrice)}")
-
-def det_mat(matrice,righe,col): #calcolo determinante della matrice se righe==col
-    if righe==col:
-        print(f"Il determinante è: {np.linalg.det(matrice)}")
-    else:
-        print("Righe e colonne diverse. Non si può calcolare il determinante")
-
-def molt_elem_wise(mat,righe,col): #moltiplicazione element-wise
-    mat2=np.random.randint(0,100,size=(righe,col))
-    molt=mat*mat2
-    print(f"Seconda matrice generata:\n{mat2}")
-    print(f"Moltiplicazione delle due matrici:\n {molt}")
-
-def media_mat(matrice): #calcolo media della matrice
-    print(f"La media della matrice è: {np.mean(matrice)}")
+main()      #richiamo al main
