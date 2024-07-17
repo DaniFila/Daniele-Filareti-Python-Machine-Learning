16adb37e18ec9d4f7897ef3b8fdbe327ab300428
DaniFila <daniele.filareti@icloud.com>
2024-07-17 17:08:43 +0200

    Create Es2_pandas.py

---
+++ Corso Python/Mercoledì 17/Es2_pandas.py
@@ -0,0 +1,61 @@
+import Es2
+
+def scelte_menu(): # funzione menù che stampa le opzioni e chiede all'utente la scelta
+        info = """\n1: Visualizza DataFrame
+2: Aggiungi colonna Totale Vendite
+3: Raggruppa i dati per Prodotto e calcola il totale delle vendite per ciascun prodotto
+4: Trova il prodotto più venduto in termini di Quantità
+5: Identifica la città con il maggior volume di vendite totali
+6: Crea un nuovo DataFrame che mostri solo le vendite superiori a un certo valore
+7: Ordina il DataFrame originale per la colonna "Totale Vendite" in ordine decrescente
+8: Visualizza il numero di vendite per ogni città
+0: Exit
+"""
+        scelta = input(info)
+        return scelta
+    
+
+def main(): # funzione main che richiama la classe menù e la classe Vendite del modulo Es2
+    df = Es2.Vendite()
+    df.genera_dataframe()
+    while True:
+        scelta = scelte_menu()
+        if scelta == "1":
+            print(f"DataFrame:\n{df.df}")
+        elif scelta == "2":
+            df.aggiungi_totale_vendite()
+        elif scelta == "3":
+            df.raggruppa_vendite_per_prodotto()
+        elif scelta == "4":
+            try:  # verifica per l'operazione che la colonna Vendite Totali sia creata
+                df.prodotto_più_venduto_su_quantità()
+            except:
+                print("Colonna Vendite Totali non creata!")
+        elif scelta == "5":
+            try: # verifica per l'operazione che la colonna Vendite Totali sia creata
+                df.città_con_maggior_volume_vendite_tot()
+            except:
+                print("Colonna Vendite Totali non creata!")
+        elif scelta == "6":
+            try: # verifica per l'operazione che la colonna Vendite Totali sia creata
+                df.nuovo_dataframe_filtrato()
+            except:
+                print("Colonna Vendite Totali non creata!")
+        elif scelta == "7":
+            try: # verifica per l'operazione che la colonna Vendite Totali sia creata
+                df.ordina_tot_vendite_desc()
+            except:
+                print("Colonna Vendite Totali non creata!")
+        elif scelta == "8":
+            try: # verifica per l'operazione che la colonna Vendite Totali sia creata
+                df.visualizza_vendite_per_città()
+            except:
+                print("Colonna Vendite Totali non creata!")
+        elif scelta == "0":
+            print("Arrivederci!")
+            break
+        else:
+            print("Scelta non valida")
+
+
+main()
