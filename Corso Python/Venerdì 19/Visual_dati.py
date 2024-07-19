ad5abc5c585643caa0cafcc765d83d049840965e
DaniFila <daniele.filareti@icloud.com>
2024-07-19 15:40:14 +0200

    Create Visual_dati.py

---
+++ Corso Python/Venerdì 19/Visual_dati.py
@@ -0,0 +1,9 @@
+import matplotlib as plt
+import seaborn as sns
+
+
+plt.rcParams['figure.figsize'] = [10, 6]  
+# Imposta la dimensione predefinita delle figure
+
+plt.rcParams['figure.dpi'] = 100          
+# Imposta la risoluzione delle figure in DPI
