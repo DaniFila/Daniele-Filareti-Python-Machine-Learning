b3cb83318184da966c13308b4e8ea06b0f4ca139
DaniFila <daniele.filareti@icloud.com>
2024-07-19 17:00:33 +0200

    Create Es1_Pandas_Visual.py

---
+++ Corso Python/Venerdì 19/Es1_Pandas_Visual.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Temperature:
+    def __init__(self):
+        self.df = self.crea_df()
+
+    def crea_df(self):
+        data = {"Temperature":np.random.randint(0,40,30)}
+        df =pd.DataFrame(data)
+        return df
+    
+    def temp_max(self):
+        max = self.df["Temperature"].idxmax()
+        return max
+    def temp_min(self):
+        min = self.df["Temperature"].idxmin()
+        return min
+    def temp_mean(self):
+        mean = self.df["Temperature"].mean()
+        return mean
+    def temp_median(self):
+        median = self.df["Temperature"].median()
+        return median
+    def print_stats(self):
+        max = self.temp_max()
+        min = self.temp_min()
+        mean = self.temp_mean()
+        median = self.temp_median()
+        cat = ["MAX","MIN","MEAN","MEDIAN"]
+        val = [max,min,mean,median]
+        plt.figure()
+        plt.bar(cat, val)
+        plt.title('STATISTICHE')
+        plt.xlabel('TIPOLOGIA')
+        plt.ylabel('Valori')
+        plt.show()
+
+a = Temperature()
+
+a.print_stats()
+    
