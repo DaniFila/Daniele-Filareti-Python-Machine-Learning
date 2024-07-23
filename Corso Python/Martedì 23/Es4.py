73774ce479096c1505316ed5d2cc0f796e476e13
DaniFila <daniele.filareti@icloud.com>
2024-07-23 18:02:11 +0200

    Update Es4.py

--- Corso Python/Martedì 23/Es4.py
+++ Corso Python/Martedì 23/Es4.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 class Classe_Studenti:
     def __init__(self):
-        self.df = self.crea_df()
+        self.df = self.dataframe_demo()
     
     def crea_df(self):
         mesi = ['Mese1', 'Mese2', 'Mese3', 'Mese4', 'Mese5', 'Mese6']
@@ -27,10 +27,42 @@ class Classe_Studenti:
         df = pd.DataFrame(data)
         return df
     
-    def visualizza_stats(self):
+    def dataframe_demo(self):
+        data=[]
+        mesi = ['Mese1', 'Mese2', 'Mese3', 'Mese4', 'Mese5', 'Mese6']
+        studenti = ["Peppino","Antonio"]
+        for nome in studenti:
+            for mese in mesi:
+                voto_ita = np.random.randint(1,11)
+                voto_stor = np.random.randint(1,11)
+                voto_mate = np.random.randint(1,11)
+                voto_ingl = np.random.randint(1,11)
+                data.append({"Nome":nome,"Mese":mese,"Italiano":voto_ita,"Storia":voto_stor,"Inglese":voto_ingl,"Matematica":voto_mate})
+        df = pd.DataFrame(data)
+        return df
+
+    def calcola_stats(self):
         medie_df = self.df.groupby(["Nome","Mese"])[["Italiano","Storia","Inglese","Matematica"]].mean()
         medie_df["Media"] = medie_df.mean(axis=1)
         medie_df.reset_index(inplace=True)
+        
+        stud1_df = medie_df.loc[medie_df["Nome"] == "Peppino"]
+        stud2_df = medie_df.loc[medie_df["Nome"] == "Antonio"]
+        stud2_df.reset_index(inplace=True)
+        stud1_df.reset_index(inplace=True)
+
+        return stud1_df,stud2_df
+    
+    def visualizza_stats_subplot(self):
+        stud1_df,stud2_df = self.calcola_stats()
+        plt.figure()
+        plt.subplot(2,1,1)
+        plt.plot(stud1_df["Media"])
+        plt.title("Media dell'alunno Peppino")
+        plt.subplot(2,1,2)
+        plt.plot(stud2_df["Media"])
+        plt.title("Media dell'alunno Antonio")
+        plt.show()
         
 
 
