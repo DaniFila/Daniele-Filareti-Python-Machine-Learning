408d3f996b8c5c392d41b4a6b5596377485ef502
DaniFila <daniele.filareti@icloud.com>
2024-07-25 15:32:48 +0200

    Create Es4_Machine_Learning.py

---
+++ Corso Python/Giovedì 25/Es4_Machine_Learning.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from sklearn.metrics import r2_score,mean_squared_error
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.metrics import PredictionErrorDisplay
+
+
